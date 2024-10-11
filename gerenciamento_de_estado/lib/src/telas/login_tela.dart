fe0e883c267dc993fb203ffcbd6b4e6fb315a695
Rodrigo Bossini <professorbossini@gmail.com>
2024-10-11 12:51:22 -0300

    ap02(ger-estado): pg 31
    - email_validator instalado
    - retomar implementando o mixin

--- gerenciamento_de_estado/lib/src/telas/login_tela.dart
+++ gerenciamento_de_estado/lib/src/telas/login_tela.dart
@@ -5,11 +5,22 @@ class LoginTela extends StatelessWidget{
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(20.0),
-      child: const Column(
+      child: Column(
         //4. Encaixar os dois métodos, ou seja, exibir os dois campos
         children: [
-          Text('Filho 1'),
-          Text('Filho 2')  
+          emailField(),
+          passwordField(),
+          Container(
+            margin: EdgeInsets.only(top: 12.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: submitButton()
+                )
+              ],
+            ),
+          )
+            
         ],
       ),
     );
@@ -18,9 +29,29 @@ class LoginTela extends StatelessWidget{
   //1. restringir o teclado para que ele seja apropriado para a digitação de e-mail
   //2. Exibir uma dica instruindo o usuário a digitar o seu e-mail
   Widget emailField(){
-    return TextField();
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'seu@email.com',
+        labelText: 'Digite seu e-mail'
+      ),
+    );
   }
-
   //3. Escrever um método semelhante ao método emailField que produz um Widget próprio para a exibição de um campo para senha
-
+  Widget passwordField(){
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Senha',
+        labelText: 'Digite sua senha'
+      ),
+    );
+  }
   //5. implementar um método para produzir um botão
+  Widget submitButton(){
+    return ElevatedButton(
+      onPressed: (){}, 
+      child: Text('Login')
+    );
+  }
+}
