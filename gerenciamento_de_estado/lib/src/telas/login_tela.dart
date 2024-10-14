c0f8d7e57e68fa8959b905283405dcc53a293052
Rodrigo Bossini <professorbossini@gmail.com>
2024-10-14 09:41:48 -0300

    ap02(flutter): ger estado
    - pg 20

---
+++ gerenciamento_de_estado/lib/src/telas/login_tela.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class LoginTela extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          emailField(),
+          passwordField(),
+          Container(
+            margin: EdgeInsets.only(top: 12.0),
+            child: submitButton()
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget emailField(){
+    return TextField(
+      //1. restringir o tipo do teclado para que ele suba de maneira apropriada para a digitação de e-mails
+      keyboardType: TextInputType.emailAddress,
+      //2. exibir um texto explicando para o usuário o que ele deve digitar, ou seja, uma espécie de placeholder
+      decoration: InputDecoration(
+        hintText: 'seu@email.com',
+        labelText: "Digite seu e-mail"
+      ),
+    );
+  }
+
+  Widget passwordField(){
+    return TextField(
+      //ocultar o texto, fazendo as bolinhas aparecerem quando o usuário digita
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'senha',
+        labelText: "Digite sua senha"
+      ),
+    );
+  }
+
+  Widget submitButton(){
+    return ElevatedButton(
+      onPressed : (){}, 
+      child: Text('Login')
+    );
+  }
+}
+
