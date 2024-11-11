63d74a40a3fa6a8cc13098b5e9aed6241aa8362b
Rodrigo Bossini <professorbossini@gmail.com>
2024-11-11 11:04:08 -0300

    ap01(django): encerra a apostila

--- django/primeiro_projeto/primeira_aplicacao/views.py
+++ django/primeiro_projeto/primeira_aplicacao/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+#HttpRequest -> processamento -> HttpResponse
+def index(request):
+  #poderiamos acessar o modelo aqui, para acionar regras de negócio
+  variaveis = {
+    'minha_primeira_variavel': "Hello, variáveis depois de alterar o diretório!!!"
+  }
+  return render(request, 'primeira_aplicacao/index.html', context=variaveis)
+  # return HttpResponse('Hello, Django!')
--- django/primeiro_projeto/primeiro_projeto/settings.py
+++ django/primeiro_projeto/primeiro_projeto/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -52,10 +53,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "primeiro_projeto.urls"
 
+TEMPLATE_DIR = BASE_DIR / Path('templates')
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [TEMPLATE_DIR],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -115,7 +117,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+STATIC_DIR = BASE_DIR / Path('static')
+STATICFILES_DIRS = [
+  STATIC_DIR
+]
 STATIC_URL = "static/"
 
 # Default primary key field type
