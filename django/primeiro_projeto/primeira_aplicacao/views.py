045c095eda922b6e05c2dbdb1e8e3a25cc02ff2b
Rodrigo Bossini <professorbossini@gmail.com>
2024-11-11 12:50:32 -0300

    ap01(django): encerra a apostila

--- django/primeiro_projeto/primeira_aplicacao/views.py
+++ django/primeiro_projeto/primeira_aplicacao/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+  variaveis = {
+    'minha_primeira_variavel': 'hello, variáveis!'
+  }
+  return render(request, 'primeira_aplicacao/index.html', variaveis)
--- django/primeiro_projeto/primeiro_projeto/settings.py
+++ django/primeiro_projeto/primeiro_projeto/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -52,10 +53,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "primeiro_projeto.urls"
 
+TEMPLATE_DIR = BASE_DIR / Path('templates')
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [TEMPLATE_DIR],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -116,6 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_DIR = BASE_DIR / Path('static')
+STATICFILES_DIRS = [
+  STATIC_DIR
+]
 STATIC_URL = "static/"
 
 # Default primary key field type
